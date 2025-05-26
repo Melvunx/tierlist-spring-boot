bb18095b98c8aa860a2e94092167a309103f5bb0
Melvunx <augustine.melv@gmmail.com>
2025-05-26 17:31:17 +0200

    First commit

---
+++ src/main/java/com/melvunx/tierlist/TierlistApplication.java
@@ -0,0 +1,12 @@
+package com.melvunx.tierlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TierlistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TierlistApplication.class, args);
+    }
+}
---
+++ src/test/java/com/melvunx/tierlist/TierlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melvunx.tierlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TierlistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
